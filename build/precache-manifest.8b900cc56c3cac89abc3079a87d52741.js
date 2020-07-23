ffb5c7e011a8b8d9f3d4d4c8ebbac610d95b3c47
azdobnov <moonday@asia.com>
2020-07-23 13:53:09 +0300

    builded

---
+++ build/precache-manifest.8b900cc56c3cac89abc3079a87d52741.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f5a638ea666e231b28b197faa425206",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2c37527b82a0bd79349e",
+    "url": "/static/css/main.d0a422b6.chunk.css"
+  },
+  {
+    "revision": "f23775758145afb53ec8",
+    "url": "/static/js/4.1b9265b4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/4.1b9265b4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fc8f7dcfed8ad1e69a35",
+    "url": "/static/js/Auth.2d2a6387.chunk.js"
+  },
+  {
+    "revision": "e95c26f5cc54190cefbe",
+    "url": "/static/js/Opening.3c7e9267.chunk.js"
+  },
+  {
+    "revision": "2c37527b82a0bd79349e",
+    "url": "/static/js/main.2babf939.chunk.js"
+  },
+  {
+    "revision": "14465c93ce28e75dc1df",
+    "url": "/static/js/runtime-main.e8190e63.js"
+  }
+]);
